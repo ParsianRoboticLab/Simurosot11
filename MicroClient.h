781d88545b1e802ff5fa9fa83412b308d702a081
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2019-07-18 12:05:24 +0430

    add micro clinet

---
+++ MicroClient.h
@@ -0,0 +1,40 @@
+// MicroClient.h : main header file for the MICROCLIENT application
+//
+
+#ifndef MICROCLIENT_HEADER
+#define MICROCLIENT_HEADER
+
+
+#ifndef __AFXWIN_H__
+	#error include 'stdafx.h' before including this file for PCH
+#endif
+
+#include "resource.h"       // main symbols
+
+/////////////////////////////////////////////////////////////////////////////
+// CMicroClientApp:
+// See MicroClient.cpp for the implementation of this class
+//
+
+class CMicroClientApp : public CWinApp
+{
+public:
+	CMicroClientApp();
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CMicroClientApp)
+	public:
+	virtual BOOL InitInstance();
+	//}}AFX_VIRTUAL
+
+// Implementation
+	//{{AFX_MSG(CMicroClientApp)
+	afx_msg void OnAppAbout();
+		// NOTE - the ClassWizard will add and remove member functions here.
+		//    DO NOT EDIT what you see in these blocks of generated code !
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+#endif // MICROCLIENT_HEADER
